55427c0479616b5fecec831d96d965485e4b85fd
kelly <85955516+kellyjohanasalb@users.noreply.github.com>
2023-02-03 14:46:19 -0500

    cambiando HOCs por react hooks
    haciendo pequeños cambios

--- src/ChangeAlert/useStorageListener.js
+++ src/ChangeAlert/useStorageListener.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
-function useStorageListener(synchronize) {
-    const [storageChange, setStorageChange] = React.useState(false);
-
-    window.addEventListener('storage', (change) => {
-        if (change.key === 'TODOS_V1') {
-            console.log('Hubo cambios en TODOS_V1');
-            setStorageChange(true);
-        }
-    });
-
-    const toggleShow = () => {
-        synchronize();
-        setStorageChange(false);
+function useStorageListener(sincronize) {
+  const [storageChange, setStorageChange] = React.useState(false);
+  
+  window.addEventListener('storage', (change) => {
+    if (change.key === 'TODOS_V1') {
+      console.log('Hubo cambios en TODOS_V1');
+      setStorageChange(true);
     }
-    
-    return {
-        show: storageChange,
-        toggleShow,
-    };
-}
+  });
 
+  const toggleShow = () => {
+    sincronize();
+    setStorageChange(false);
+  };
+
+  return {
+    show: storageChange,
+    toggleShow,
+  };
+}
 
 export { useStorageListener };
